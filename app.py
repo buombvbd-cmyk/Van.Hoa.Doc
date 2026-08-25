0f45a59b0af8b94f0400647dfed308643d8b9fab
buombvbd-cmyk <buombv.bd@truongxanh.edu.vn>
2026-08-25 10:42:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory, abort
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
